4c5c980aa6e42f25bf337ac7641b426820655aa2
Riya Savant <riyasavant@Riyas-MacBook-Air.local>
2023-07-26 23:39:01 +0530

    Added sidebar categories

--- app/components/Accordian/Accordian.js
+++ app/components/Accordian/Accordian.js
@@ -4,17 +4,19 @@ import { useState } from "react";
 import Image from "next/image";
 import Arrow from '../../icons/arrow.png';
 
-function Accordian({ title, children }) {
+function Accordian({ title, children, size = 'small', isNested = false }) {
     const [isActive, setIsActive] = useState(false);
 
     const handleClick = () => {
         setIsActive((prev) => !prev);
     };
 
+    const textSize = size === 'small' ? 'text-xs' : 'text-sm';
+
     return (
-        <div className="w-full border-b border-slate-200 p-4">
-            <div className="flex items-center justify-between py-2 cursor-pointer" onClick={handleClick}>
-                <h3 className="text-xs">{title}</h3>
+        <div className={`w-full ${!isNested ? 'border-b border-slate-200 p-4' : 'p-1'}`}>
+            <div className={`flex items-center justify-between py-2 cursor-pointer`} onClick={handleClick}>
+                <h3 className={textSize}>{title}</h3>
                 <span className={`${isActive ? 'transform rotate-180' : ''}`}>
                     <Image src={Arrow} width={20} height={20} />
                 </span>
--- app/components/Sidebar/Sidebar.js
+++ app/components/Sidebar/Sidebar.js
@@ -1,21 +1,35 @@
 import Wrapper from './Wrapper';
+import Accordian from '../Accordian/Accordian';
 
 export default function Sidebar({ isOpen, close }) {
   return (
     <Wrapper onClose={close} isOpen={isOpen} direction="left" showLayover>
-      <div className='px-4'>
+      <div className='px-4 divide-y overflow-y-auto h-[calc(100vh-64px)]'>
         <h1 className='p-4 pb-6 pt-6 hover:backdrop-brightness-90 flex text-sm cursor-pointer'>HOME</h1>
-        <hr />
         <h1 className='p-4 pb-6 pt-6 hover:backdrop-brightness-95 flex text-sm cursor-pointer'>NEW ARRIVALS</h1>
-        <hr />
         <h1 className='p-4 pb-6 pt-6 hover:backdrop-brightness-95 flex text-sm cursor-pointer'>BEST SELLERS</h1>
-        <hr />
-        <h1 className='p-4 pb-6 pt-6 hover:backdrop-brightness-95 flex text-sm cursor-pointer'>SHOP</h1>
-        <hr />
+        <Accordian title="SHOP" size="large">
+          <div className='px-2'>
+            <Accordian title='Mens' isNested>
+              <div className='px-2 border-l border-slate-500'>
+                <p className='text-xs pl-1 py-1.5'>T-shirts</p>
+                <p className='text-xs pl-1 py-1.5'>Shirts</p>
+                <p className='text-xs pl-1 py-1.5'>Jeans</p>
+                <p className='text-xs pl-1 py-1.5'>Sweatshirts</p>
+              </div>
+            </Accordian>
+            <Accordian title='Womens' isNested>
+            <div className='px-2 border-l border-slate-500'>
+                <p className='text-xs pl-1 py-1.5'>T-shirts</p>
+                <p className='text-xs pl-1 py-1.5'>Kurtis</p>
+                <p className='text-xs pl-1 py-1.5'>Jeans</p>
+                <p className='text-xs pl-1 py-1.5'>Sweatshirts</p>
+              </div>
+            </Accordian>
+          </div>
+        </Accordian>
         <h1 className='p-4 pb-6 pt-6 hover:backdrop-brightness-95 flex text-sm cursor-pointer'>TRACK ORDER</h1>
-        <hr />
         <h1 className='p-4 pb-6 pt-6 hover:backdrop-brightness-95 flex text-sm cursor-pointer'>RETURN REQUEST</h1>
-        <hr />
       </div>
     </Wrapper>
   );
