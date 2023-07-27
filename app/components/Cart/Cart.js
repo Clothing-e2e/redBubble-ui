0c620ec27a7ecf5c6323e47f7041ccb22447b9e4
Riya Savant <riyaasavant@gmail.com>
2023-07-27 13:08:34 +0530

    Added empty cart image

--- app/components/Cart/Cart.js
+++ app/components/Cart/Cart.js
@@ -4,6 +4,10 @@ import useStore from "../../store/store";
 import Wrapper from "../Sidebar/Wrapper";
 import Image from "next/image";
 import Counter from "../Counter/Counter";
+import utils from "@/app/utils/utils";
+import EmptyCart from '../../icons/emptyCart.png';
+
+const { isArrayEmpty } = utils;
 
 /**
  * @description Cart sidebar
@@ -19,32 +23,36 @@ function Cart() {
 
     return (
         <Wrapper showLayover={true} isOpen={isCartVisible} onClose={hideCart} title="CART">
-            <div className="p-2 px-4 overflow-y-scroll h-[calc(100vh-238px)]">
-                {cartItems.map((item) => (
-                    <div className="flex gap-2 py-4 hover:bg-slate-50">
-                        <div><Image src={`http://localhost:8080/images/${item.imagesId[0]}`} width={100} height={100} /></div>
-                        <div className="flex flex-col gap-2 ml-4 grow mr-4 justify-center">
-                            <div className="text-sm">{item.heading}</div>
-                            <div className="text-xs pt-2 pb-0"><span className="font-medium">Color:</span>{` ${item.color}`}</div>
-                            <div className="text-xs pb-2 pt-0"><span className="font-medium">Size:</span>{` ${item.size}`}</div>
-                            <div className="flex justify-between items-center">
-                                <Counter initialCount={Number(item.quantity)} id={item.id} onChange={updateCart} />
-                                <div className="text-xs font-semibold">{`Rs. ${item.price}`}</div>
+            {isArrayEmpty(cartItems) ? <div className="flex items-center w-[100%] h-[calc(100vh-77px)]"><Image src={EmptyCart} width={350} height={350} alt="Empty Cart" /></div> : (
+                <>
+                    <div className="p-2 px-4 overflow-y-scroll h-[calc(100vh-238px)]">
+                        {cartItems.map((item) => (
+                            <div className="flex gap-2 py-4 hover:bg-slate-50">
+                                <div><Image src={`http://localhost:8080/images/${item.imagesId[0]}`} width={100} height={100} /></div>
+                                <div className="flex flex-col gap-2 ml-4 grow mr-4 justify-center">
+                                    <div className="text-sm">{item.heading}</div>
+                                    <div className="text-xs pt-2 pb-0"><span className="font-medium">Color:</span>{` ${item.color}`}</div>
+                                    <div className="text-xs pb-2 pt-0"><span className="font-medium">Size:</span>{` ${item.size}`}</div>
+                                    <div className="flex justify-between items-center">
+                                        <Counter initialCount={Number(item.quantity)} id={item.id} onChange={updateCart} />
+                                        <div className="text-xs font-semibold">{`Rs. ${item.price}`}</div>
+                                    </div>
+                                </div>
+                            </div>
+                        ))}
+                    </div>
+                    <div className='absolute bottom-0 right-0 w-[100%] bg-white p-4 border-t'>
+                        <div className="py-4 px-2">
+                            <div className="flex justify-between mx-2">
+                                <p className="text-xs">SUBTOTAL</p>
+                                <p className="text-xs">{`Rs. ${subTotal}`}</p>
                             </div>
                         </div>
+                        <div className='py-4 mx-2 my-1 text-sm bg-slate-800 hover:bg-slate-700 cursor-pointer text-white rounded-md text-center'>PROCEED TO CHECKOUT</div>
+                        <div className="text-center pt-1 text-xs text-slate-500">Shipping & taxes calculated at checkout.</div>
                     </div>
-                ))}
-            </div>
-            <div className='absolute bottom-0 right-0 w-[100%] bg-white p-4 border-t'>
-                <div className="py-4 px-2">
-                    <div className="flex justify-between mx-2">
-                        <p className="text-xs">SUBTOTAL</p>
-                        <p className="text-xs">{`Rs. ${subTotal}`}</p>
-                    </div>
-                </div>
-                <div className='py-4 mx-2 my-1 text-sm bg-slate-800 hover:bg-slate-700 cursor-pointer text-white rounded-md text-center'>PROCEED TO CHECKOUT</div>
-                <div className="text-center pt-1 text-xs text-slate-500">Shipping & taxes calculated at checkout.</div>
-            </div>
+                </>
+            )}
         </Wrapper>
     )
 }
